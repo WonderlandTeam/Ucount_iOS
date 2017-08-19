8b04944217f8bd613ab92a5db6d3a9236c6c6ecb
YP Lee <1017270947@qq.com>
2017-08-19 15:17:20 +0800

    create controller

---
+++ Ucount_iOS/Ucount_IOS/Ucount_IOS/Wealth/WealthViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WealthViewController.swift
+//  Ucount_IOS
+//
+//  Created by 李一鹏 on 2017/8/19.
+//  Copyright © 2017年 李一鹏. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WealthViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
