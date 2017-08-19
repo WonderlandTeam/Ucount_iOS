f76995e17aa18f8bd7dd59cd47bf23a3887a7f60
151250059 <151250059@smail.nju.edu.cn>
2017-08-19 16:54:01 +0800

    登录界面草稿

--- Ucount_iOS/Ucount_IOS/Ucount_IOS/ViewController.swift
+++ Ucount_iOS/Ucount_IOS/Ucount_IOS/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate{
+    
+    
+    @IBOutlet weak var idText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        idText.delegate = self
+        passwordText.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    //点到view的别的地方，焦点从textfield转移,反焦点
+    override  func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        idText.resignFirstResponder()
+        passwordText.resignFirstResponder()
+    }
+    
+    //点击return，键盘消失
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{ // called when 'return' key pressed. return NO to ignore.
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
