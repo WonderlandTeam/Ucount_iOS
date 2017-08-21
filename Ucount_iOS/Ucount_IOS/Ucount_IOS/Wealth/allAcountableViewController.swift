d18a0bda4d9342866879f325286395254824cd5b
YP Lee <1017270947@qq.com>
2017-08-21 12:01:56 +0800

    tableview不显示

--- Ucount_iOS/Ucount_IOS/Ucount_IOS/Wealth/allAcountableViewController.swift
+++ Ucount_iOS/Ucount_IOS/Ucount_IOS/Wealth/allAcountableViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class allAcountableViewController: UITableViewController {
+    
+    @IBOutlet var acountTableview: UITableView!
+  
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Uncomment the following line to preserve selection between presentations
-//        // self.clearsSelectionOnViewWillAppear = false
-//
-//        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-//    }
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
 
 //    override func didReceiveMemoryWarning() {
 //        super.didReceiveMemoryWarning()
