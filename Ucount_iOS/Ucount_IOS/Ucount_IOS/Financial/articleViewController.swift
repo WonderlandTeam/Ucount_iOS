06227a433d62a7cac26bda9bf0e7781bfa73f0cc
151250059 <151250059@smail.nju.edu.cn>
2017-08-30 21:40:16 +0800

    自适应

--- Ucount_iOS/Ucount_IOS/Ucount_IOS/Financial/articleViewController.swift
+++ Ucount_iOS/Ucount_IOS/Ucount_IOS/Financial/articleViewController.swift
@@ -71,7 +71,7 @@ class articleViewController: UIViewController {
     @IBAction func talkTapped(_ sender: Any) {
 
         //这里根据post.id来找到所有的评论，并传给commentViewController
-        var comments = [Comment(postId: post.id, userId: "userid1", date: "2017-3-18 22:00", text: "文章写得很好。"),
+        var comments = [Comment(postId: post.id, userId: "userid1", date: "2017-3-18 22:00", text: "文章写得很好。这个人之前写过很多有用的干活，希望大家都踊跃一些去找他啊"),
                        Comment(postId: post.id, userId: "userid2", date: "2013-12-1 22:20", text: "给你加个鸡腿！！"),
                        Comment(postId: post.id, userId: "userid3", date: "2012-11-28 12:00", text: "哈哈哈")]
 
@@ -90,6 +90,8 @@ class articleViewController: UIViewController {
             }
         }else{//有评论
             let vc = UIStoryboard(name: "Financial", bundle: nil).instantiateViewController(withIdentifier: "commentViewController") as! commentViewController
+            vc.comments = comments
+            
             self.present(vc,animated: true){
                 
             }
--- Ucount_iOS/Ucount_IOS/Ucount_IOS/Financial/commentViewController.swift
+++ Ucount_iOS/Ucount_IOS/Ucount_IOS/Financial/commentViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class commentViewController: UIViewController {
+class commentViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
-    var comments = [Comment(postId: "", userId: "", date: "", text: "")]
+    var comments:[Comment] = []
+    @IBOutlet weak var table: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.table.estimatedRowHeight = 80
+        self.table.rowHeight = UITableViewAutomaticDimension
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,4 +25,24 @@ class commentViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return comments.count
+        //return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = self.table.dequeueReusableCell(withIdentifier: "firstCell")!
+        
+        //cell里要显示的东西
+        (cell.viewWithTag(100) as! UILabel).text = comments[indexPath.row].userId
+        (cell.viewWithTag(101) as! UILabel).text = comments[indexPath.row].date
+        (cell.viewWithTag(102) as! UILabel).text = comments[indexPath.row].text
+        cell.accessoryType = .disclosureIndicator
+        // cell.editingAccessoryType = .detailButton
+        
+        return cell
+    }
+
 }
