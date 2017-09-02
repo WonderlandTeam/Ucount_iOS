c3f68d4babe97511f388fa8fd5724e6fff46c25d
151250059 <151250059@smail.nju.edu.cn>
2017-09-03 00:02:03 +0800

    规划的滑动

---
+++ Ucount_iOS/Ucount_IOS/Ucount_IOS/Plan/PageViewExtension_plan.swift
@@ -0,0 +1,38 @@
+//
+//  PageViewExtension_plan.swift
+//  Ucount_IOS
+//
+//  Created by 黄飘 on 2017/9/2.
+//  Copyright © 2017年 李一鹏. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension PlanViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?{
+        if viewController is SaveViewController
+        {
+            return budgetView
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?{
+        
+        
+        if viewController is BudgetViewController
+        {
+            return budgetView
+        }
+        
+        return nil
+        
+    }
+    
+}
+
+
+
